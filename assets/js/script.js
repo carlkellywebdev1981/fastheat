9ad087c35202b2f88fe114fbae250add444d1c14
Carl Kelly <carlkelly@Carls-MBP.home>
2017-01-31 13:01:42 +0000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+/* ----------------------------------------------------------------------------------------------------- */
+/* animate.css */
+/* ----------------------------------------------------------------------------------------------------- */
+
+
+$(document).ready(function() {
+                jQuery('.post').addClass("hideme").viewportChecker({
+                    classToAdd: 'visible animated fadeIn', // Class to add to the elements when they are visible
+                    offset: 200    
+                   }); 
+                $('.animated').attr('style', 'visibility: visible !important;');
+            });  
+
+
+/* ----------------------------------------------------------------------------------------------------- */
+/* nav scroll */
+/* ----------------------------------------------------------------------------------------------------- */
+
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+  });
+});
+
